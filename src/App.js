c9f164e972f821c69e2b54fe27646dd9874b8445
BELLORI <bellori729@gmail.com>
2024-03-27 02:32:18 +0900

    [기능] 일정 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
@@ -21,9 +21,24 @@ const App = () => {
       checked: false,
     },
   ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo)); // 기존 배열에 todo 데이터를 이어붙여 새로운 배열로 반환
+      nextId.current += 1;
+    },
+    [todos],
+  );
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,15 +2,27 @@ import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 import { useCallback, useState } from 'react';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState('');
 
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
 
+  // onClick으로 하고 button에다가 추가해도 처리는 할 수 있으나,
+  // 인풋에서 Enter키를 눌러버리면 onSubmit 이벤트가 발생되기 때문에 onSubmit으로 처리하는 것이다.
+  // 만약 onClick으로 한다면 onKeyPress 이벤트를 통해 Enter를 감지하는 로직을 또 추가해주어야 한다.
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault(); // 버튼의 기본 기능(새로고침) 차단
+      onInsert(value); // text로 들어갈 내용을 현재 input 값에 입력된 value로 전달
+      setValue(''); // value 값 초기화
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 적어주세요."
         value={value}
