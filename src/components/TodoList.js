1346e1c8b2e809fbb3e851eba2db4f1ece0351e7
BELLORI <bellori729@gmail.com>
2024-03-27 02:41:36 +0900

    [기능] 지우기 기능 구현

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,11 @@
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.key} />
+        <TodoListItem todo={todo} key={todo.key} onRemove={onRemove} />
       ))}
     </div>
   );
