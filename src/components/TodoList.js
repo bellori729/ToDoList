70d54703eaed2ef33d82784e0476213afde11500
BELLORI <bellori729@gmail.com>
2024-03-28 21:33:36 +0900

    [기능] react-virtualized 사용

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
           todo={todo}
-          key={todo.key}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [todos, onRemove, onToggle],
+  );
+  return (
+    <List
+      className="TodoList"
+      width={512} // TodoListItem 각 요소 너비 512px
+      height={513} // TodoListItem 각 요소 1개 당 높이 57px * 9
+      rowCount={todos.length} // 요소 개수
+      rowHeight={57} // 요소 높이
+      rowRenderer={rowRenderer} // 요소를 렌더링할 때 쓰는 함수
+      list={todos}
+      style={{ outline: 'none' }} // List에 기본 적용되는 outline 제거
+    />
   );
 };
 
