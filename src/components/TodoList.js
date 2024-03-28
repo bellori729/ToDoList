4054a09e135f696ec4100096de021ea7510260c6
BELLORI <bellori729@gmail.com>
2024-03-28 20:42:59 +0900

    [기능] list 컴포넌트 최적화

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
 
@@ -16,4 +17,5 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
   );
 };
 
-export default TodoList;
+// 부모 요소인 App 컴포넌트에서 다른 state가 추가되어 해당 값들이 업데이트 될 때에는 TodoList 컴포넌트가 불필요한 리렌더링이 발생되는데 React.memo를 활용하여 이를 방지
+export default React.memo(TodoList);
